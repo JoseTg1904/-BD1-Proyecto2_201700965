1fb46b95a78bbc5b22cc8c2820ad70bde04a4296
Jose Carlos I Alonzo Colocho <58497257+JoseTg1904@users.noreply.github.com>
2020-12-28 18:27:34 -0600

    modelo, ddl, carga
    Agregado el entidad relacion propuesto para el enunciado 1, el ddl que genera dicho modelo y las consultas de carga de datos tanto masiva como el de tabla temporal hacia el modelo

---
+++ Enunciado1/CargaDeDatos.sql
@@ -0,0 +1,62 @@
+#utilizando la base de datos para el enunciado 1
+USE enunciado1Proyecto2;
+
+/*Para la carga del csv se debe de mover dicho csv hacia la carpeta segura
+la ruta en cual se debe de mover este archivo se visualiza con el comando 
+SHOW VARIABLES LIKE "secure_file_priv";
+*/
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/Enunciado1.csv'
+INTO TABLE Temporal
+COLUMNS TERMINATED BY ';' LINES TERMINATED BY '\n' IGNORE 1 LINES 
+(nombre_compania, contacto_compania, correo_compania, telefono_compania,
+tipo, nombre,  correo, telefono, @casteo, direccion, ciudad, codigo_postal, 
+region, producto, categoria_producto, cantidad, precio_unitario);
+
+#carga de datos a tablas sin dependencias
+INSERT INTO TipoPersona (tipoPersona) 
+SELECT DISTINCT tipo FROM Temporal;
+
+INSERT INTO Region (region)
+SELECT DISTINCT region FROM Temporal;
+
+INSERT INTO CodigoPostal (codigoPostal)
+SELECT DISTINCT codigo_postal FROM Temporal;
+
+INSERT INTO TipoProducto (tipoProducto)
+SELECT DISTINCT categoria_producto FROM Temporal;
+
+INSERT INTO Compania (nombre, contacto, correo, telefono)
+SELECT DISTINCT nombre_compania, contacto_compania, correo_compania, telefono_compania
+FROM Temporal;
+
+#carga de datos a tablas con dependencia
+INSERT INTO Ciudad (ciudad, ID_Region)
+SELECT DISTINCT ciudad, 
+(SELECT ID_Region FROM Region WHERE Region.region = Temporal.region)
+FROM Temporal;
+
+INSERT INTO Persona (nombre, correo, telefono, fechaRegistro, direccion, 
+ID_CodigoPostal, ID_Ciudad, ID_TipoPersona)
+SELECT DISTINCT nombre, correo, telefono, fecha_registro, direccion,
+(SELECT ID_CodigoPostal FROM CodigoPostal WHERE codigoPostal = codigo_postal),
+(SELECT ID_Ciudad FROM Ciudad WHERE Ciudad.ciudad = Temporal.ciudad),
+(SELECT ID_TipoPersona FROM TipoPersona WHERE tipoPersona = tipo)
+FROM Temporal;
+
+INSERT INTO Producto (nombre, precio, ID_TipoProducto)
+SELECT DISTINCT producto, precio_unitario,
+(SELECT ID_TipoProducto FROM TipoProducto WHERE tipoProducto = categoria_producto)
+FROM Temporal;
+
+INSERT INTO Transaccion (ID_Compania, ID_Persona)
+SELECT DISTINCT 
+(SELECT ID_Compania FROM Compania WHERE Compania.nombre = Temporal.nombre_compania),
+(SELECT ID_Persona FROM Persona WHERE Persona.nombre = Temporal.nombre)
+FROM Temporal;
+
+INSERT INTO DetalleTransaccion (cantidad, ID_Compania, ID_Persona, ID_Producto)
+SELECT cantidad,
+(SELECT ID_Compania FROM Compania WHERE Compania.nombre = Temporal.nombre_compania),
+(SELECT ID_Persona FROM Persona WHERE Persona.nombre = Temporal.nombre),
+(SELECT ID_Producto FROM Producto WHERE Producto.nombre = Temporal.producto)
+FROM Temporal;
---
+++ Enunciado1/InstruccionesDDL.sql
@@ -0,0 +1,105 @@
+#Creando la base de datos para almacenar el modelo
+CREATE DATABASE enunciado1Proyecto2;
+
+#utilizando la base de datos creada
+USE enunciado1Proyecto2;
+
+#Creando las tablas que no tengan dependencia de otras
+CREATE TABLE Region(
+	ID_Region INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    region VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE TipoPersona(
+	ID_TipoPersona INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    tipoPersona CHAR(1)
+);
+
+CREATE TABLE CodigoPostal(
+	ID_CodigoPostal INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    codigoPostal VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE Compania(
+	ID_Compania INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    contacto VARCHAR(100) NOT NULL,
+    correo VARCHAR(100) NOT NULL,
+    telefono VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE TipoProducto(
+	ID_TipoProducto INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    tipoProducto VARCHAR(75) NOT NULL
+);
+
+#creando las tablas que tienen dependencias
+CREATE TABLE Ciudad(
+	ID_Ciudad INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    ciudad VARCHAR(100) NOT NULL,
+    ID_Region INT NOT NULL,
+    FOREIGN KEY (ID_Region) REFERENCES Region(ID_Region)
+);
+
+CREATE TABLE Persona(
+	ID_Persona INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	nombre VARCHAR(100) NOT NULL,
+    correo VARCHAR(100) NOT NULL,
+    telefono VARCHAR(50) NOT NULL,
+    fechaRegistro DATE NOT NULL,
+    direccion VARCHAR(100) NOT NULL,
+    ID_CodigoPostal INT NOT NULL,
+    ID_Ciudad INT NOT NULL,
+    ID_TipoPersona INT NOT NULL,
+    FOREIGN KEY (ID_CodigoPostal) REFERENCES CodigoPostal(ID_CodigoPostal),
+    FOREIGN KEY (ID_Ciudad) REFERENCES Ciudad(ID_Ciudad),
+    FOREIGN KEY (ID_TipoPersona) REFERENCES TipoPersona(ID_TipoPersona)
+);
+
+CREATE TABLE Producto(
+	ID_Producto INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    precio DECIMAL(6, 2) NOT NULL,
+    ID_TipoProducto INT NOT NULL,
+    FOREIGN KEY (ID_TipoProducto) REFERENCES TipoProducto(ID_TipoProducto)
+);
+
+CREATE TABLE Transaccion(
+	ID_Persona INT NOT NULL,
+    ID_Compania INT NOT NULL,
+    PRIMARY KEY (ID_Compania, ID_Persona),
+    FOREIGN KEY (ID_Persona) REFERENCES Persona(ID_Persona),
+    FOREIGN KEY (ID_Compania) REFERENCES Compania(ID_Compania)
+);
+
+CREATE TABLE DetalleTransaccion(
+	ID_DetalleTransaccion INT AUTO_INCREMENT PRIMARY KEY,
+    cantidad INT NOT NULL,
+    ID_Producto INT NOT NULL,
+    ID_Persona INT NOT NULL,
+    ID_Compania INT NOT NULL,
+    FOREIGN KEY (ID_Producto) REFERENCES Producto(ID_Producto),
+    FOREIGN KEY (ID_Persona) REFERENCES Persona(ID_Persona),
+    FOREIGN KEY (ID_Compania) REFERENCES Compania(ID_Compania)
+);
+
+#tabla temporal que sirve para cargar el csv y llenar el modelo
+CREATE TABLE Temporal(
+	nombre_compania VARCHAR(100) NOT NULL,
+    contacto_compania VARCHAR(100) NOT NULL,
+    correo_compania VARCHAR(100) NOT NULL,
+    telefono_compania VARCHAR(50) NOT NULL,
+    tipo CHAR(1) NOT NULL,
+    nombre VARCHAR(100) NOT NULL,
+    correo VARCHAR(100) NOT NULL,
+    telefono VARCHAR(100) NOT NULL,
+    fecha_registro DATE NOT NULL,
+    direccion VARCHAR(100) NOT NULL,
+    ciudad VARCHAR(100) NOT NULL,
+    codigo_postal INT NOT NULL,
+    region VARCHAR(100) NOT NULL,
+    producto VARCHAR(100) NOT NULL,
+    categoria_producto VARCHAR(100) NOT NULL,
+    cantidad INT NOT NULL,
+	precio_unitario DECIMAL(6, 2) NOT NULL
+);
